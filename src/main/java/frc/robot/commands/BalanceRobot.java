66d38e9f8eb68e9b6615c428be96870d6c8b2c52
rmeesters <meesters.rich@gmail.com>
2023-02-10 18:26:36 -0500

    Simple code to balance - no PID

--- src/main/java/frc/robot/commands/BalanceRobot.java
+++ src/main/java/frc/robot/commands/BalanceRobot.java
@@ -3,14 +3,43 @@ package frc.robot.commands;
 
 import frc.robot.Constants;
 import frc.robot.subsystems.Swerve;
+import edu.wpi.first.math.geometry.Translation2d;
+import com.kauailabs.navx.frc.AHRS;
 
 
 public class BalanceRobot {
-    private double initalAngle=0;
+    private double initialAngle;
     private Swerve s_Swerve;
+    private AHRS gyro;
+    
     public BalanceRobot(){
         s_Swerve  = new Swerve();
+        initialAngle = gyro.getRoll();
 
+        while (initialAngle > 2.0 || initialAngle < -2.0) {
+            if (initialAngle > 0.0){
+                s_Swerve.drive(
+                    new Translation2d(0.1, 0.0).times(Constants.Swerve.maxSpeed), 
+                    0.0 * Constants.Swerve.maxAngularVelocity, 
+                 false, 
+                 true);
+            }
+            else if (initialAngle < 0.0) {
+                s_Swerve.drive(
+                    new Translation2d(-0.1, 0.0).times(Constants.Swerve.maxSpeed), 
+                    0.0 * Constants.Swerve.maxAngularVelocity, 
+                 false, 
+                 true);    
+            }
+            else {
+                s_Swerve.drive(
+                    new Translation2d(0.0, 0.0).times(Constants.Swerve.maxSpeed), 
+                    0.0 * Constants.Swerve.maxAngularVelocity, 
+                 false, 
+                 true);
+            }
+        }
+  
     }
     
     // public double angleOffset(){
