8d92d37e16289c8ae2d4c0252e6094bd0a51ab3d
Berdan S <berdanstamov05@gmail.com>
2023-02-20 12:45:52 -0500

    feb 20th balancerobot.java
    balance robot, removed the move functions and have it only put data on the dashboard for now

--- src/main/java/frc/robot/commands/BalanceRobot.java
+++ src/main/java/frc/robot/commands/BalanceRobot.java
@@ -10,6 +10,7 @@ import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.smartdashboard.*;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
@@ -21,7 +22,8 @@ import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 public class BalanceRobot {
     private Swerve s_Swerve;
     private final double SPEED_BALANCING = 1;
-    final private double TOLERANCE_VALUE = 2.0; //the tolerance val is 2 degrees
+    final private double TOLERANCE_VALUE = 2.0;
+    boolean robotBalanced; //the tolerance val is 2 degrees
     //TrajectoryConfig config;
     public BalanceRobot(){
         s_Swerve  = new Swerve();
@@ -36,39 +38,48 @@ public class BalanceRobot {
 
 
     public void balanceSwerve(){
-
-        while(isRobotBalanced()==false){
+        SmartDashboard.putBoolean("Is robot balanced", robotBalanced);
+        SmartDashboard.putNumber("Roll Val", s_Swerve.gyro.getRoll());
 
             if(s_Swerve.gyro.getRoll()> TOLERANCE_VALUE){
-           
-                s_Swerve.drive(
-                    new Translation2d(0.1, 0.0).times(SPEED_BALANCING ), 
-                    0.0 * SPEED_BALANCING, 
-                 false, 
-                 true);
+              robotBalanced=false;
+              
+                // s_Swerve.drive(
+                //     new Translation2d(0.1, 0.0).times(SPEED_BALANCING ), 
+                //     0.0 * SPEED_BALANCING, 
+                //  false, 
+                //  true);
             
             //  GO FORWARDS
             }
 
-            else if(s_Swerve.gyro.getRoll()< TOLERANCE_VALUE){
+            else if(s_Swerve.gyro.getRoll()< -(TOLERANCE_VALUE)){
 
-                s_Swerve.drive(
-                    new Translation2d(-0.1, 0.0).times(SPEED_BALANCING), 
-                    0.0 * SPEED_BALANCING, 
-                 false, 
-                 true);
+                robotBalanced=false;
+
+            }
+
+            else {
+                
+                robotBalanced=true;
+
+                // s_Swerve.drive(
+                //     new Translation2d(-0.1, 0.0).times(SPEED_BALANCING), 
+                //     0.0 * SPEED_BALANCING, 
+                //  false, 
+                //  true);
                 //GO BACKWARDS
             }
 
 
-        }
+        
     }
     
-    public boolean isRobotBalanced(){
+    // public boolean isRobotBalanced(){
 
-        if(s_Swerve.gyro.getRoll()>TOLERANCE_VALUE ||s_Swerve.gyro.getRoll()<TOLERANCE_VALUE)return false;
-        else return true;
-    }
+    //     if(s_Swerve.gyro.getRoll()>TOLERANCE_VALUE ||s_Swerve.gyro.getRoll()<TOLERANCE_VALUE)return false;
+    //     else return true;
+    // }
     
 
 }
